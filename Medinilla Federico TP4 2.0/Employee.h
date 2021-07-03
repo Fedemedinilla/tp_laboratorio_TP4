d7b09df4ccfac684b5fe1cadf7adb1ec03747ea9
Federico Medinilla <68014417+Fedemedinilla@users.noreply.github.com>
2021-07-03 17:09:23 -0300

    Segunda entrega del TP4, arregle un par de cosas

---
+++ Medinilla Federico TP4 2.0/Employee.h
@@ -0,0 +1,35 @@
+#ifndef employee_H_INCLUDED
+#define employee_H_INCLUDED
+typedef struct
+{
+    int id;
+    char nombre[128];
+    int horasTrabajadas;
+    int sueldo;
+}Employee;
+
+Employee* employee_new();
+Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr, char* sueldoStr);
+void employee_delete();
+
+int employee_setId(Employee* this,int id);
+int employee_getId(Employee* this,int* id);
+
+int employee_setNombre(Employee* this,char* nombre);
+int employee_getNombre(Employee* this,char* nombre);
+
+int employee_setHorasTrabajadas(Employee* this,int horasTrabajadas);
+int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas);
+
+int employee_setSueldo(Employee* this,int sueldo);
+int employee_getSueldo(Employee* this,int* sueldo);
+
+int employee_buscarUltimoId(LinkedList* pArrayListEmployee,int* ultimoId);
+int employee_buscarId(LinkedList* pArrayListEmployee,int idBuscado);
+Employee* employee_buscarXid(LinkedList* pArrayListEmployee,int idBuscado,int* indiceParaMododificar);
+Employee* employee_menuModificion(Employee* emp);
+int employee_sortByName(void* a,void* b);
+int employee_sortByHoras(void* a,void* b);
+int employee_sortBySueldo(void* a,void* b);
+
+#endif // employee_H_INCLUDED
