04b9ed150d5420e04f75c95f8d594d294ab283b8
Federico Medinilla <68014417+Fedemedinilla@users.noreply.github.com>
2021-07-03 17:10:39 -0300

    Delete Medinilla Federico TP4 directory

--- Medinilla Federico TP4/parser.h
+++
@@ -1,8 +0,0 @@
-#ifndef PARSER_H_INCLUDED
-#define PARSER_H_INCLUDED
-#include "LinkedList.h"
-
-int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee);
-int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee);
-
-#endif // PARSER_H_INCLUDED
